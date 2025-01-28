9cafe9b1fc49e942ecf8157bd443218b74509fa5
Carlos Tirado <mariotiradotovar@outlook.com>
2025-01-27 23:19:24 -0500

    secreto

--- config/firebase.js
+++ config/firebase.js
@@ -1,16 +1,20 @@
-const firebaseAdmin = require("firebase-admin");
+// firebaseConfig.js
+require("dotenv").config(); // Carga las variables de entorno desde el archivo .env
+const admin = require("firebase-admin"); // Importa el SDK de Firebase Admin
 
-let app;
-if (!firebaseAdmin.apps.length) {
-    app = firebaseAdmin.initializeApp({
-        credential: firebaseAdmin.credential.cert(
-            require("../../sandra-77bcf-firebase-adminsdk-fbsvc-b0ae33bf48.json")
-        ),
-    });
-} else {
-    app = firebaseAdmin.app(); // Usa la app ya inicializada
-}
+// Crea el objeto de servicio utilizando las variables de entorno
+const serviceAccount = {
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"), // Asegura que los saltos de línea estén correctos
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    clientId: process.env.FIREBASE_CLIENT_ID,
+};
 
-const db = firebaseAdmin.firestore();
+// Inicializa Firebase Admin con las credenciales del archivo .env
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+});
 
-module.exports = { firebaseAdmin, db };
+// Exporta la instancia de admin para usarla en otros archivos si es necesario
+module.exports = admin;
